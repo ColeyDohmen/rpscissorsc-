6755230dc7ca82d1114a09a19d911de9aa3590af
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-05 13:37:54 -0600

    start

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace rpscissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool running = true;
+            while (running)
+            {
+                System.Console.WriteLine("Let's play rock, paper, scissors!");
+            }
+        }
+    }
+}
