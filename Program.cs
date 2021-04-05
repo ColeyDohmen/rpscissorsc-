5cfe1786bce883e7dd77da78718549f81d095a34
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-05 13:52:25 -0600

    playtime

--- Program.cs
+++ Program.cs
@@ -6,12 +6,24 @@ namespace rpscissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
 
             bool running = true;
             while (running)
             {
-                System.Console.WriteLine("Let's play rock, paper, scissors!");
+                System.Console.WriteLine(@"Would you like to play rock, paper, scissors?
+                Y / N");
+                ConsoleKeyInfo userInput = System.Console.ReadKey();
+                Console.Clear();
+                if (userInput.Key == ConsoleKey.Y)
+                {
+                    System.Console.WriteLine("YAY! Let's play!");
+                }
+                else if (userInput.Key == ConsoleKey.N)
+                {
+                    System.Console.WriteLine("Sad days, maybe next time");
+                    running = false;
+                }
             }
         }
     }
