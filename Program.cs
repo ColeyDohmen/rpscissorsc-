87194417fc9f9e4621f1d978d05ab8698baacedd
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-05 14:16:41 -0600

    rock finished

--- Program.cs
+++ Program.cs
@@ -18,6 +18,29 @@ namespace rpscissors
                 if (userInput.Key == ConsoleKey.Y)
                 {
                     System.Console.WriteLine("YAY! Let's play!");
+                    System.Console.WriteLine(@"Do you choose 
+                    (R)ock
+                    (P)aper
+                    or
+                    (S)cissors?");
+                    ConsoleKeyInfo usersInput = System.Console.ReadKey();
+                    Console.Clear();
+                    if (usersInput.Key == ConsoleKey.R)
+                    {
+                        System.Console.WriteLine("You chose Rock, PC chose Rock, you tie!");
+                    }
+                    else if (usersInput.Key == ConsoleKey.P)
+                    {
+                        System.Console.WriteLine("You chose Paper, PC chose Rock, you win!");
+                    }
+                    else if (usersInput.Key == ConsoleKey.S)
+                    {
+                        System.Console.WriteLine("You chose Scissors, PC chose Rock, you lose!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("invalid input");
+                    }
                 }
                 else if (userInput.Key == ConsoleKey.N)
                 {
